43e749e4daff86805a7270ce2a68cd2b8ee2b12f
Kate Raylyan <31958491+kotiknalune@users.noreply.github.com>
2017-09-29 14:08:44 +0300

    Update guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,18 +1,22 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.guessing;
+    }
     setRange(min, max) {
-      this.min = min;
-      this.max = max;
+        this.min = min;
+        this.max = max;
     }
     guess() {
-      return this.min == this.max ? this.min : Math.ceil(this.min + (this.max - this.min) / 2);
+        this.guessing = Math.round((this.max - this.min) / 2);
+        return this.guessing;
     }
     lower() {
-      this.max = this.guess();
+        this.max = this.guessing;
     }
     greater() {
-      this.min = this.guess();
+        this.min = -this.guessing;
     }
 }
 module.exports = GuessingGame;
